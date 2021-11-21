82b76651eed9319fb21258de770350bd2b20e95d
Marharyta Kulinich <margaret.kulinich@gmail.com>
2021-11-21 11:13:50 +0200

    Updates

---
+++ precache-manifest.c34bd2e7bc7e8485595bce4b7f6ac156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a347075c59fdda85489b6ebcce5404",
+    "url": "/Cursor-HW-margaretkulinich-React/index.html"
+  },
+  {
+    "revision": "72252b863475e830d4c3",
+    "url": "/Cursor-HW-margaretkulinich-React/static/css/main.c707a19e.chunk.css"
+  },
+  {
+    "revision": "3d8e8da96b133cc4195a",
+    "url": "/Cursor-HW-margaretkulinich-React/static/js/2.87ceb1c8.chunk.js"
+  },
+  {
+    "revision": "12d98599ee75686ece6379c641ecd705",
+    "url": "/Cursor-HW-margaretkulinich-React/static/js/2.87ceb1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d61cabde358ef9f3db",
+    "url": "/Cursor-HW-margaretkulinich-React/static/js/3.8b6da2d0.chunk.js"
+  },
+  {
+    "revision": "72252b863475e830d4c3",
+    "url": "/Cursor-HW-margaretkulinich-React/static/js/main.632e976b.chunk.js"
+  },
+  {
+    "revision": "5494ad102c2e1ef0708e",
+    "url": "/Cursor-HW-margaretkulinich-React/static/js/runtime-main.436ec738.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Cursor-HW-margaretkulinich-React/precache-manifest.c34bd2e7bc7e8485595bce4b7f6ac156.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cursor-HW-margaretkulinich-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
